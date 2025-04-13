c2521ebd69aca4ddfc6c63d00da58e92577e57fd
DavitHakobyan01 <david.hakobyan1237@gmail.com>
2025-04-13 11:13:27 -0700

    added functionality of search button

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,46 @@
 const container = document.getElementById('catalogContainer');
+const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
 
-artists.forEach(artist => {
+for (let i = 0; i < artists.length; i++) {
+  const artist = artists[i];
+
   const card = document.createElement('div');
   card.classList.add('artist-card');
 
   card.innerHTML = `
     <img src="assets/img/${artist.name}.jpg" alt="${artist.name}">
     <h3>${artist.name}</h3>
-    <p><strong>Popular Song:</strong> ${artist.song}</p>
+    <p><strong>Popular Song:</strong> ${artist.popularSong}</p>
     <p><strong>Age:</strong> ${artist.age}</p>
-    <p><strong>From:</strong> ${artist.origin}</p>
+    <p><strong>From:</strong> ${artist.hometown}</p>
+    <p><strong>Fun fact::</strong> ${artist.funFact}</p>
   `;
 
   container.appendChild(card);
+}
+
+searchButton.addEventListener('click', function () {
+  const keyword = searchInput.value.toLowerCase();
+  container.innerHTML = '';
+
+  for (let i = 0; i < artists.length; i++) {
+    const artist = artists[i];
+
+    if (artist.name.toLowerCase().includes(keyword)) {
+      const card = document.createElement('div');
+      card.classList.add('artist-card');
+
+      card.innerHTML = `
+        <img src="assets/img/${artist.name}.jpg" alt="${artist.name}">
+        <h3>${artist.name}</h3>
+        <p><strong>Popular Song:</strong> ${artist.popularSong}</p>
+        <p><strong>Age:</strong> ${artist.age}</p>
+        <p><strong>From:</strong> ${artist.hometown}</p>
+        <p><strong>Fun fact::</strong> ${artist.funFact}</p>
+      `;
+
+      container.appendChild(card);
+    }
+  }
 });
-
-
-
-
-
